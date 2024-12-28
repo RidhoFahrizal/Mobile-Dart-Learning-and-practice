e65452cf4afe01234f71715da56a386c0c22a319
RidhoFahrizal <farizalridho@gmail.com>
2024-12-28 21:21:08 +0700

    menambahkan kode di main.dart

---
+++ main.dart
@@ -0,0 +1,15 @@
+
+
+
+void main() {
+  var frontName = 'Ridho';
+  var lastName = 'Fahrizal';
+  var height = 178.9;
+
+  print('Hallo ' + frontName + lastName);
+  if (height > 170) {
+    print('Wah kamu tinggi juga ya!');
+  } else {
+    print('Oh ternyata kamu pendek ya ');
+  }
+}
